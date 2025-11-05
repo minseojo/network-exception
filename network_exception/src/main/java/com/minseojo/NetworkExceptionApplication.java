6d0d477b725b799348153ead8dc01c9906428a74
minseo Jo <liging12@naver.com>
2025-11-05 23:03:45 +0900

    init

---
+++ network_exception/src/main/java/com/minseojo/NetworkExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.minseojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkExceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkExceptionApplication.class, args);
+	}
+
+}
---
+++ network_exception/src/test/java/com/minseojo/NetworkExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minseojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
